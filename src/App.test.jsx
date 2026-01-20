0e022a88143e247cf5a3b43d9893f623b7b6e694
Emrah KOLEMENOGLU <emrahkolemenoglu@gmail.com>
2026-01-20 16:00:02 +0300

    break test intentionally

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders CI/CD Demo title", () => {
   render(<App />);
-  expect(screen.getByText("CI/CD Demo")).toBeInTheDocument();
+  expect(screen.getByText("CI/CD DEMO")).toBeInTheDocument();
 });
-expect(screen.getByText("CI/CD DEMO")).toBeInTheDocument();
+
